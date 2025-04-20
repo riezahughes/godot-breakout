2cbac3f4155ab375fc7a9877b214fa7ed7398c49
riezahughes <dan@tantalus-troupe.com>
2025-04-20 23:11:54 +0200

    bouncing! (kinda)

--- Classes/ball.gd
+++ Classes/ball.gd
@@ -1,21 +1,47 @@
 extends Node2D
+
 signal out_of_bounds
+signal wall_hit
+
 @export var ball_speed = 5
-# Called when the node enters the scene tree for the first time.
+
 var velocity = Vector2.ZERO
 var screen_size
-
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	var rng = RandomNumberGenerator.new()
-	velocity.x = 50
-	velocity.y = 50
+	rotate(randf_range(-180, 180))
+	#velocity.x = 50
+	#velocity.y = 20
 
+#func bounce(x_pos, y_pos):
+	#print(velocity)
+	#if position.y == 0:
+		#rotate(180)
+		#velocity.x = x_pos - randf_range(-100, 100)
+		#velocity.y  = y_pos - randf_range(0, 100)
+		
+	#if position.x == 0:
+		#rotate(180)
+		#velocity.x = x_pos - randf_range(0, 100)
+		#velocity.y  = y_pos - randf_range(0, 100)
+	
+	#if position.x >= screen_size.x:
+		#rotate(180)
+		#velocity.x = x_pos - randf_range(0, 100)
+		#velocity.y  = y_pos - randf_range(-100, 100)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position += velocity * delta * ball_speed
-	position = position.clamp(Vector2.ZERO, screen_size)
+	#position += velocity * ball_speed * delta
+	#position = position.clamp(Vector2.ZERO, screen_size)
 	
 	if position.y == screen_size.y:
 		out_of_bounds.emit()
+#func bounce():
+	#veloci
+
+func _on_area_entered(area: Area2D) -> void:
+	#bounce(velocity.x, velocity.y)
+	wall_hit.emit()
+	
--- MainScenes/main_game.gd
+++ MainScenes/main_game.gd
@@ -1,19 +1,81 @@
 extends Node
 
+var game_has_been_reset = true
+var ball_velocity = Vector2.ZERO
+var screen_size
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$MainPlayer.position = $BatStartPosition.global_position 
-	$MainBall.position = $BallStartPosition.global_position
-
+	print("READY")
+	screen_size = Vector2(1200, 800)
+	_reset()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	# Ball Setup
+	if game_has_been_reset == false:
+		$MainBall.position += ball_velocity * $MainBall.ball_speed * delta
+		$MainBall.position = $MainBall.position.clamp(Vector2.ZERO, screen_size)
 	
-func _reset_ball():
-	$MainBall.position = $BallStartPosition.global_position
+	# Timer display on HUD
+	$HUD/TimerCounter.text = str("%0.2f" % $StartTimer.time_left," s")
 
+	# Start the game is the game has been reset. Should only run oncee
+	if $StartTimer.is_stopped() && game_has_been_reset == true:
+		print("Running start game in process")
+		game_has_been_reset = false
+		start_game()
+	
+	
+func _reset():
+	print("Resetting_Game");	
+	print($BallStartPosition.position);
+	$MainPlayer.position = $BatStartPosition.global_position 
+	$MainBall.position = $BallStartPosition.position
+	print($BallStartPosition.position)
+	$HUD/TimerCounter.text = "3"
+	$StartTimer.start(1)
+	$MainBall.hide()
+	$MainPlayer.hide()
+	$HUD/TimerCounter.show()
+	$HUD/MainMessageBox.show()
+	stop_ball();
+	game_has_been_reset = true
+	
+func start_game():
+	print("Starting_Game");
+	$MainBall.show()
+	
+	ball_velocity = ball_velocity.rotated(randf_range(0, 90))
+	
+	$MainPlayer.show()
+	$HUD/TimerCounter.hide()
+	$HUD/MainMessageBox.hide()
+	
+func stop_ball():
+	ball_velocity.x = 50
+	ball_velocity.y = 20
 
 func _on_main_ball_out_of_bounds():
-	_reset_ball()
+	print("OUT OF BOUNDS")
+	print($MainBall.position)
+	_reset()
+	
+
+
+func _on_level_boundary_area_entered(area: Area2D) -> void:
+	if $MainBall.position.x >= screen_size.x:
+		print("Hit the right")
+		print(ball_velocity)
+		ball_velocity = ball_velocity.rotated(90)
+	elif $MainBall.position.x <= 0:
+		print("Hit the left")
+		print(ball_velocity)
+		ball_velocity = ball_velocity.rotated(-90)
+	elif $MainBall.position.y <= 0:
+		print("Hit the top")
+		print(ball_velocity)
+		ball_velocity = ball_velocity.rotated(90)
+	else:
+		print("NOPE")
+		print($MainBall.position.y)
