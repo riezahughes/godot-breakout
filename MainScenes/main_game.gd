1a2c98b7e4b92ef4944db1b5fb7fb8e159a3b3ff
riezahughes <dan@tantalus-troupe.com>
2025-04-21 12:16:52 +0200

    its alive?

--- MainScenes/main_game.gd
+++ MainScenes/main_game.gd
@@ -2,6 +2,8 @@ extends Node
 
 var game_has_been_reset = true
 var ball_velocity = Vector2.ZERO
+var lives = 2
+var level = 1
 var screen_size
 
 # Called when the node enters the scene tree for the first time.
@@ -18,6 +20,8 @@ func _process(delta):
 	
 	# Timer display on HUD
 	$HUD/TimerCounter.text = str("%0.2f" % $StartTimer.time_left," s")
+	$HUD/LivesLabel.text = str("Lives: ", lives)
+	$HUD/LevelLabel.text = str("Level: ", level)
 
 	# Start the game is the game has been reset. Should only run oncee
 	if $StartTimer.is_stopped() && game_has_been_reset == true:
@@ -26,26 +30,39 @@ func _process(delta):
 	
 	
 func _reset():
-	print("Resetting the Game...");	
-	print($BallStartPosition.position);
+	if lives > 0:
+		print("Resetting the Game...");	
+		print($BallStartPosition.position);
+		$MainPlayer.position = $BatStartPosition.global_position 
+		$MainBall.position = $BallStartPosition.position
+		print($BallStartPosition.position)
+		$HUD/TimerCounter.text = "3"
+		$StartTimer.start(1)
+		$CurrentBlocks.hide()
+		$MainBall.hide()
+		$MainPlayer.hide()
+		$HUD/TimerCounter.show()
+		$HUD/MainMessageBox.show()
+		stop_ball()
+		game_has_been_reset = true
+	else: 
+		game_over()
+	
+func game_over():
 	$MainPlayer.position = $BatStartPosition.global_position 
 	$MainBall.position = $BallStartPosition.position
-	print($BallStartPosition.position)
-	$HUD/TimerCounter.text = "3"
-	$StartTimer.start(1)
+	$CurrentBlocks.hide()
 	$MainBall.hide()
 	$MainPlayer.hide()
-	$HUD/TimerCounter.show()
+	$HUD/MainMessageBox.text = "Game Over!"
 	$HUD/MainMessageBox.show()
-	stop_ball();
-	game_has_been_reset = true
 	
 func start_game():
 	print("Starting the game...");
 	$MainBall.show()
 	
 	ball_velocity = ball_velocity.rotated(randf_range(0, 90))
-	
+	$CurrentBlocks.show()
 	$MainPlayer.show()
 	$HUD/TimerCounter.hide()
 	$HUD/MainMessageBox.hide()
@@ -55,8 +72,10 @@ func stop_ball():
 	ball_velocity.y = 50
 
 func _on_main_ball_out_of_bounds():
-	print("OUT OF BOUNDS")
-	_reset()
+	if lives > 0:
+		print("OUT OF BOUNDS")
+		lives -= 1
+		_reset()
 
 func _on_level_boundary_area_entered(area: Area2D) -> void:
 	if(area.name == "MainPlayer"):
@@ -66,10 +85,19 @@ func _on_level_boundary_area_entered(area: Area2D) -> void:
 		ball_velocity = ball_velocity.bounce(posNormalise)
 	else:
 		ball_velocity = ball_velocity.reflect(posNormalise)
+		
+
 
 
 func _on_main_player_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
 	if area.name == "LevelBoundary":
 		return
 	var posNormalise = $MainPlayer.position.normalized()
-	ball_velocity = ball_velocity.bounce(posNormalise)
+	ball_velocity = ball_velocity.bounce(-posNormalise)
+
+
+func _on_target_block_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	if area.name == "LevelBoundary":
+		return
+	var posNormalise = $MainPlayer.position.normalized()
+	ball_velocity = ball_velocity.reflect(posNormalise)
